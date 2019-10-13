edcf76d4c56f45a32eee8f533ce09d51f6c4030a
deks23 <deks232@gmail.com>
2019-10-13 23:18:05 +0200

    Fixed Azure configuration

--- src/main/java/pl/sopmproject/sopmserver/controller/UserController.java
+++ src/main/java/pl/sopmproject/sopmserver/controller/UserController.java
@@ -1,14 +1,30 @@
 package pl.sopmproject.sopmserver.controller;
 
+
+import com.microsoft.applicationinsights.TelemetryClient;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
+    private static final Logger logger = LogManager.getLogger(UserController.class);
 
-    @RequestMapping(value = "/login")
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login(){
+        logger.info("Login request: ");
         return "LOGIN TEST";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String register(){
+        logger.info("Register request: ");
+        return "register";
     }
 }
