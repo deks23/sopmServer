cf3ad6d1f275052438ba0433e7525bf8b7224df3
deks23 <deks232@gmail.com>
2019-11-16 14:48:16 +0100

    Deploy 0.3 version

--- src/main/java/pl/sopmproject/sopmserver/model/request/UpdateDataRequest.java
+++ src/main/java/pl/sopmproject/sopmserver/model/request/UpdateDataRequest.java
@@ -8,6 +8,6 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 public class UpdateDataRequest {
-    private LocalDateTime birthDate;
+    private LocalDateTime birthday;
     private String gender;
 }
